bc3af23ec95406aa07edc3dd3f3f1cf2afb7555b
Daniel Yu <dcyu93@gmail.com>
2013-09-21 02:10:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textacademy::Application.config.secret_token = 'f802db23efa5b05b836b63b5f5758864b023841690a739365e5acd573fff35135c80fb10c94b322121dc9b26a2c7ca93eb7ce2866050795227a3c85e511a7cd1'
