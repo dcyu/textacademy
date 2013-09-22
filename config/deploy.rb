5d6ac83b28603adb5971c9d9aaa4149372f8dc1c
Shaan Sapra <ssapra@uchicago.edu>
2013-09-21 23:47:06 -0400

    using new vm for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,19 @@
 require "bundler/capistrano"
- 
+
 set :application, "textacademy"
 set :user, "ssapra"
- 
+
 set :scm, :git
 set :repository, "git@github.com:dcyu/textacademy.git"
 set :branch, "master"
 set :use_sudo, true
- 
-server "137.116.246.140", :web, :app, :db, primary: true
- 
+
+server "137.116.232.170", :web, :app, :db, primary: true
+
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
- 
+
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
@@ -21,14 +21,14 @@ namespace :deploy do
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
- 
+
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     sudo "mkdir -p #{shared_path}/config"
   end
   after "deploy:setup", "deploy:setup_config"
- 
+
   task :symlink_config, roles: :app do
     # Add database config here
   end
